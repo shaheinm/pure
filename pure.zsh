f28e98095f74e075e63a5174b200ebd9a6654e2a
Shahein Moussavi <shahein.moussavi@turner.com>
2019-01-25 17:35:37 -0500

    test

--- pure.zsh
+++ pure.zsh
@@ -115,7 +115,11 @@ prompt_pure_preprompt_render() {
 	local -a preprompt_parts
 
 	# Set the path.
-	preprompt_parts+=('%F{blue}%c%f')
+	if [[ $FULLDIR == "true" ]]; then
+		preprompt_parts+=('%F{blue}%~%f')
+	else
+		preprompt_parts+=('%F{blue}%c%f')
+	fi
 
 	# Add git branch and dirty status info.
 	typeset -gA prompt_pure_vcs_info
